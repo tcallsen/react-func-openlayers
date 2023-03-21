75f4df4a2a1265bd892e620e26e895ea82bd3791
Taylor Callsen <tcallsen@adobe.com>
2023-03-21 07:16:48 -0600

    updating dependencies and to latest ReactDOMClient api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const container = document.getElementById('root');
+
+// Create a root - https://github.com/reactwg/react-18/discussions/5
+const root = ReactDOMClient.createRoot(container);
+
+// Initial render: Render an element to the root
+root.render(
+  <App />
 );
